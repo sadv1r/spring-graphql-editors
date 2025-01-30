c84462a456e18877c96f0d56434dd140d5eea185
sadv1r <5000487+sadv1r@users.noreply.github.com>
2025-01-30 18:57:56 +0300

    Add support for custom HTTP headers in Voyager configuration

--- graphql-voyager-spring-boot-starter/src/main/java/ru/sadv1r/spring/graphql/editor/voyager/configuration/VoyagerProperties.java
+++ graphql-voyager-spring-boot-starter/src/main/java/ru/sadv1r/spring/graphql/editor/voyager/configuration/VoyagerProperties.java
@@ -3,6 +3,8 @@ package ru.sadv1r.spring.graphql.editor.voyager.configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
+import java.util.Map;
+
 /**
  * Configuration properties for the Voyager GraphQL editor.
  *
@@ -27,6 +29,8 @@ public class VoyagerProperties {
      * Hide settings panel
      */
     private boolean hideSettings = false;
+
+    private Map<String, String> headers = Map.of();
 
     private Cdn cdn = Cdn.JSDELIVR;
 
@@ -86,6 +90,14 @@ public class VoyagerProperties {
 
     public void setStylePath(String stylePath) {
         this.stylePath = stylePath;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
     }
 
     private static class DisplayOptions {
